8333bc2a3fb696371bb82e13c2caf80b4d0ac010
damienPilat <dspilat@hotmail.com>
2021-03-20 00:07:22 +0000

    Reflect datatype change (obj -> array)

--- scripts/custom/dynamicDevice.js
+++ scripts/custom/dynamicDevice.js
@@ -10,22 +10,25 @@ function populateDevice() {
     // Get all containers with 'device' classname
     let deviceContainers = document.getElementsByClassName('device');
 
+    // Loop through all devices the current container
     Array.from(deviceContainers).forEach(container => {
+        console.log("containreID:", container.id);
         let deviceID = container.id.split('-')[0];
         let deviceType = container.id.split('-')[1];
+
         // Check if id in list
-            console.log("DevicID:", deviceID);
         if (deviceID in sectionDetails) {
             // Get individual container
             // Loop through all entries of device type, creating img w/ attributes
-            for (const elData of Object.values(sectionDetails[deviceID]['device'][deviceType])) {
+
+            sectionDetails[deviceID]['device'][deviceType].forEach(elData => {
                 let deviceElement = document.createElement('img');      // Creat img tag
                 deviceElement.className = deviceType;                   // Add attributes
                 deviceElement.id = deviceID + '-' + deviceType + '-' + elData;
                 deviceElement.src = mediaPaths[deviceType] + deviceID + '-' + deviceType + '-' + elData + '.png';
                 deviceElement.alt = makeAlt(deviceID, elData, deviceType);
                 container.appendChild(deviceElement);                   // Append to container
-            }
+            });
         }
     });
 } // END: populateTech
