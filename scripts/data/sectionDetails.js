2b01df0cf238b4a2d1000488225f3a0c0e570675
damienPilat <dspilat@hotmail.com>
2021-03-21 23:27:39 +0000

    Data Structure updated to support multilingual

--- scripts/data/sectionDetails.js
+++ scripts/data/sectionDetails.js
@@ -23,14 +23,14 @@ let sectionDetails = {
                                 type: 'h2',
                                 className: 'sectionTitle',
                                 id: null,
-                                content: 'Movie Dashboard',
+                                content: {en: 'Movie Dashboard', fr: 'Tableau de Bord - Film'},
                                 children: null
                             },
                             1: {
                                 type: 'p',
                                 className: 'desc-text',
                                 id: null,
-                                content: "Interactive dashboard of the all-time highest grossing movies, automatically updated through API calls, and made responsive for all devices. Built in pure HTML/CSS, made interactive with JavaScript, and created custom artwork with Adobe XD.",
+                                content: {en: "Interactive dashboard of the all-time highest grossing movies, automatically updated through API calls, and made responsive for all devices. Built in pure HTML/CSS, made interactive with JavaScript, and created custom artwork with Adobe XD.", fr: 'A venir'},
                                 children: null
                             }
                         },
@@ -98,14 +98,14 @@ let sectionDetails = {
                                         type: 'h2',
                                         className: 'sectionTitle',
                                         id: null,
-                                        content: 'Member space & E-commerce - Diligent Eye',
+                                        content: {en: 'Member space & E-commerce - Diligent Eye', fr: 'A venir'},
                                         children: null
                                     },
                                     1: {
                                         type: 'p',
                                         className: 'desc-text',
                                         id: null,
-                                        content: 'Design and Implementation of Diligent Eye’s Member space, including product management and e-commerce integration. Built on WordPress platform, with custom JavaScript.',
+                                        content: {en: 'Design and Implementation of Diligent Eye’s Member space, including product management and e-commerce integration. Built on WordPress platform, with custom JavaScript.', fr: 'A venir'},
                                         children: null
                                     }
                                 }
@@ -206,7 +206,7 @@ let sectionDetails = {
                                 type: 'h2',
                                 className: 'sectionTitle col-sm-6',
                                 id: null,
-                                content: 'Dining Experience - Mobile App',
+                                content: {en: 'Dining Experience - Mobile App', fr: 'A venir'},
                                 children: null
                             },
                             1: {
@@ -219,7 +219,7 @@ let sectionDetails = {
                                         type: 'p',
                                         className: 'desc-text col-sm-6',
                                         id: null,
-                                        content: 'Re-imagined covid and post-pandemic dining experience through an in-depth User Experience Research strategy, resulting into an interactive prototype crafted in Figma.',
+                                        content: {en: 'Re-imagined covid and post-pandemic dining experience through an in-depth User Experience Research strategy, resulting into an interactive prototype crafted in Figma.', fr: 'A venir'},
                                         children: null
                                     },
                                     1: {
@@ -274,7 +274,7 @@ let sectionDetails = {
                                 type: 'h2',
                                 className: 'sectionTitle',
                                 id: null,
-                                content: 'WordPress',
+                                content: {en: 'WordPress', fr: 'WordPress'},
                                 children: null
                             },
                             1: {
