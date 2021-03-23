0e7885d93bf90dfbbdd19fe5a19fc2b029c2ddab
damienPilat <dspilat@hotmail.com>
2021-03-23 22:43:43 +0000

    ChangeColorMode supports multiple DOM elements

--- scripts/custom/darkmodeSwitch.js
+++ scripts/custom/darkmodeSwitch.js
@@ -6,25 +6,35 @@ function modeSwitch() {
     mode_switch.addEventListener('change', function () {
         if (this.checked) {
             // Change all elements with '.bgColor_light' classname
-            changeColorMode('.bgColor_light');
+            changeColorMode('bgColor_light');
         } else {
             // Change all element with '.<>_dark' classname
-            changeColorMode('.bgColor_dark');
+            changeColorMode('bgColor_dark');
         }
     });
 }
+
 
 // Get current selector color, then get all elements w/ selector.
 // Replace current color with opposite
 function changeColorMode(selector) {
     let currentColor = selector.split('_')[1];
     try {
-        let domElements = document.querySelector(selector).classList;
-        domElements.replace(selector.substring(1), selector.split('_')[0].substring(1)+'_'+  getOppositeColor(currentColor));
+        let domElements = document.getElementsByClassName(selector);
+        // No element found test
+        if (domElements.length < 1) { console.log("Couldn't find Dom element with className " + selector); }
+
+        // Loop through all found elements
+        Array.from(domElements).forEach(element => {
+            let list = element.classList;
+            list.replace(selector, selector.split('_')[0]+'_'+  getOppositeColor(currentColor));
+        });
+        // domElements.replace(selector.substring(1), selector.split('_')[0].substring(1)+'_'+  getOppositeColor(currentColor));
     } catch (e) {
         console.log("Could not change " + currentColor + " mode to " + getOppositeColor(currentColor) + " mode.");
     }
 }
+
 
 // Returns opposite color to input
 function getOppositeColor(currentColor) {
