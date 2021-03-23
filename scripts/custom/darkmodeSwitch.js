dd7d26285fdb43c332c88b6250d1affe23e66113
damienPilat <dspilat@hotmail.com>
2021-03-23 22:35:34 +0000

    Function to change color based on className

--- scripts/custom/darkmodeSwitch.js
+++ scripts/custom/darkmodeSwitch.js
@@ -5,11 +5,28 @@ function modeSwitch() {
     let mode_switch = document.getElementById('modeSwitch');
     mode_switch.addEventListener('change', function () {
         if (this.checked) {
-            document.body.style.backgroundColor = '#3a3a3a';
-            console.log("darkMode");
+            // Change all elements with '.bgColor_light' classname
+            changeColorMode('.bgColor_light');
         } else {
-            document.body.style.backgroundColor = '#fafafa';
-            console.log("lightMode")
+            // Change all element with '.<>_dark' classname
+            changeColorMode('.bgColor_dark');
         }
     });
+}
+
+// Get current selector color, then get all elements w/ selector.
+// Replace current color with opposite
+function changeColorMode(selector) {
+    let currentColor = selector.split('_')[1];
+    try {
+        let domElements = document.querySelector(selector).classList;
+        domElements.replace(selector.substring(1), selector.split('_')[0].substring(1)+'_'+  getOppositeColor(currentColor));
+    } catch (e) {
+        console.log("Could not change " + currentColor + " mode to " + getOppositeColor(currentColor) + " mode.");
+    }
+}
+
+// Returns opposite color to input
+function getOppositeColor(currentColor) {
+    return (currentColor === 'light') ? 'dark' : 'light';
 }
