ddabf38559896bdf9a2b74d057a34a309d5ad015
damienPilat <dspilat@hotmail.com>
2021-03-23 23:25:48 +0000

    Optimise, abstract and increase logic of Mode Switch

--- scripts/custom/darkmodeSwitch.js
+++ scripts/custom/darkmodeSwitch.js
@@ -1,48 +1,24 @@
-// Manual changing of styles based on switch setting
-// @todo change manual updates to classes change
-
+// Toggle Light/Dark Mode on Switch click
 function modeSwitch() {
-    let mode_switch = document.getElementById('modeSwitch');
-    mode_switch.addEventListener('change', function () {
-        if (this.checked) {
-            // Change all elements with '.bgColor_light' classname
-            changeColorMode('bgColor_light');
-            changeColorMode('primaryColor_light');
-            changeColorMode('secondaryColor_light');
-            changeColorMode('tertiaryColor_light');
-        } else {
-            // Change all element with '.<>_dark' classname
-            changeColorMode('bgColor_dark');
-            changeColorMode('primaryColor_dark');
-            changeColorMode('secondaryColor_dark');
-            changeColorMode('tertiaryColor_dark');
-        }
+    document.getElementById('modeSwitch').addEventListener('change', function () {
+        this.checked ? changeColorMode('light') : changeColorMode('dark');
     });
 }
 
-
-// Get current selector color, then get all elements w/ selector.
-// Replace current color with opposite
-function changeColorMode(selector) {
-    let currentColor = selector.split('_')[1];
-    try {
-        let domElements = document.getElementsByClassName(selector);
-        // No element found test
-        if (domElements.length < 1) { console.log("Couldn't find Dom element with className " + selector); }
-
-        // Loop through all found elements
-        Array.from(domElements).forEach(element => {
-            let list = element.classList;
-            list.replace(selector, selector.split('_')[0]+'_'+  getOppositeColor(currentColor));
-        });
-        // domElements.replace(selector.substring(1), selector.split('_')[0].substring(1)+'_'+  getOppositeColor(currentColor));
-    } catch (e) {
-        console.log("Could not change " + currentColor + " mode to " + getOppositeColor(currentColor) + " mode.");
-    }
+// Get DOM element by mode (light/dark), retrieve className, and appropriately update for all elements
+function changeColorMode(mode) {
+    let domElements = document.querySelectorAll("div[class$='_" + mode + "']");
+    Array.from(domElements).forEach(element => {
+        element.classList.replace(getModeClassName(element), getModeClassName(element).split('_')[0] + '_' + getOppositeColor(mode));
+    });
 }
 
+// Retrieves Mode Classname (always the last element in a list of class names)
+function getModeClassName(element) {
+    return element.className.split(" ").slice(-1)[0];
+}
 
-// Returns opposite color to input
+// Toggles the mode options (light <-> dark)
 function getOppositeColor(currentColor) {
     return (currentColor === 'light') ? 'dark' : 'light';
 }
