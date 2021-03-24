d82a89029d90eebd9928c082b1af40451d398451
damienPilat <dspilat@hotmail.com>
2021-03-24 20:00:46 +0000

    Simplify process of making Device Links & Images

--- scripts/custom/dynamicDevice.js
+++ scripts/custom/dynamicDevice.js
@@ -5,29 +5,16 @@ let mediaPaths = {
     "desktop": "media/device/desktop/"
 };
 
-// Populate devices in dom
+// Retrieve all dom elements with className 'device'
+// Loop through all elements:
+// Populate with all device images if any
 function populateDevice() {
-    // Get all containers with 'device' classname
     let deviceContainers = document.getElementsByClassName('device');
 
-    // Loop through all devices the current container
     Array.from(deviceContainers).forEach(container => {
-        let deviceID = container.id.split('-')[0];
-        let deviceType = container.id.split('-')[1];
-
-        // Check if id in list
+        let [deviceID, deviceType] = container.id.split('-');
         if (deviceID in sectionDetails) {
-            // Get individual container
-            // Loop through all entries of device type, creating img w/ attributes
-
-            sectionDetails[deviceID]['device'][deviceType].forEach(elData => {
-                let deviceElement = document.createElement('img');      // Creat img tag
-                deviceElement.className = deviceType;                   // Add attributes
-                deviceElement.id = deviceID + '-' + deviceType + '-' + elData;
-                deviceElement.src = mediaPaths[deviceType] + deviceID + '-' + deviceType + '-' + elData + '.png';
-                deviceElement.alt = makeAlt(deviceID, elData, deviceType);
-                container.appendChild(deviceElement);                   // Append to container
-            });
+            createImageElement(deviceID, deviceType, container);
         }
     });
 } // END: populateTech
