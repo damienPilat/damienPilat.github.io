44cc0dd63fc8d7b8afe378cb6cb830b9bce4608c
damienPilat <dspilat@hotmail.com>
2021-03-30 19:38:36 +0100

    Replace inline with initial Func comment

--- scripts/custom/dynamicSections.js
+++ scripts/custom/dynamicSections.js
@@ -1,44 +1,11 @@
+// Populate sections, looping through section Dict, retrieving structure and populating as approprate
 function populateSections() {
-    // Loop through entries in section list
     for (let sectionID in sectionDetails) {
-        // Create Section Container
         let domContainer = document.getElementById('content');
-
-        // Get Section Structure
         let sectionStructure = sectionDetails[sectionID].structure;
-        // Loop through structure, recursively adding sections
+
         for (let firstDepthElements in sectionStructure) {
-            // Append elements to container
             domContainer.appendChild(customElement(sectionStructure[firstDepthElements]));
         }
     }
-
 } // END: populateSections
-
-// Recursively create all children of an element
-function createChildren(children, container) {
-    for (let childrenID in children) {
-        container.append(customElement(children[childrenID]));
-    }
-}
-
-// Create a DOM element based on the json structure
-function customElement(elementDetails) {
-    // Create Dom Element
-    let element = document.createElement(elementDetails.type);
-    element.className = elementDetails.className;
-
-    // Add id, context and children IF exists
-    if (elementDetails.id !== null) {
-        element.id = elementDetails.id;
-    }
-
-    if (elementDetails.content !== null) {
-        element.innerText = elementDetails.content.en;
-    }
-
-    if (elementDetails.children !== null) {
-        createChildren(elementDetails.children, element);
-    }
-    return element;
-}
