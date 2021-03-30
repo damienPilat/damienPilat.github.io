6d37f6699629b99d34556c8da4f54c4819b514d9
damienPilat <dspilat@hotmail.com>
2021-03-30 19:39:06 +0100

    Create Helper Function Script

---
+++ scripts/custom/helperFunctions.js
@@ -0,0 +1,46 @@
+// HELPER FUNCTION FILE
+
+// DOM ELEMENTS
+// Function to create a custom element with any possible value used in project
+function customElement(config) {
+    let elementDetails = Object.assign({
+            type: null,
+            className: null,
+            id: null,
+            src: null,
+            alt: null,
+            target: null,
+            href: null,
+            innerText: null,
+            children: null
+        },
+        config
+    );
+
+    return populateElement(elementDetails);
+}
+
+// Populate DOM element with all non NULL values + Two SPECIAL cases
+// @todo: remove if statements
+function populateElement(elementDetails) {
+    let element = document.createElement(elementDetails.type);
+    for (const [key, value] of Object.entries(elementDetails)) {
+        if (value) {
+            if (key === 'children') {
+                createChildren(elementDetails.children, element);
+            } else if (key === 'innerText') {
+                element.innerText = elementDetails.innerText.en;
+            } else {
+                element[key] = value;
+            }
+        }
+    }
+    return element
+}
+
+// Recursively create all children of an element
+function createChildren(children, container) {
+    for (let childrenID in children) {
+        container.append(customElement(children[childrenID]));
+    }
+}
