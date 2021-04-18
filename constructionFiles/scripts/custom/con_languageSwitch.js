c44eb51f9ef857ff23663be50bfc509881ca31bc
damienPilat <dspilat@hotmail.com>
2021-04-18 12:06:16 +0100

    changeLanguage v2 updated to main function

--- constructionFiles/scripts/custom/con_languageSwitch.js
+++ constructionFiles/scripts/custom/con_languageSwitch.js
@@ -1,16 +1,6 @@
 // Switch languages of website
-// By replacing all page content
-function changeLanguage(e) {
-    currentLanguage = e.target.id.split("-")[1];
-
-    document.getElementById('content').innerHTML = null;
-    populateSections();
-    populateDevice();
-    populateTech();
-}
-
 // By replacing the text only
-function changeLanguage_2(e) {
+function changeLanguage(e) {
     // Update current language
     currentLanguage = e.target.id.split("-")[1];
 
