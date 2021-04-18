7db95b06e1c006b65101084b5a942511d68ad469
damienPilat <dspilat@hotmail.com>
2021-04-18 11:55:38 +0100

    v2 change Language

--- constructionFiles/scripts/custom/con_languageSwitch.js
+++ constructionFiles/scripts/custom/con_languageSwitch.js
@@ -1,8 +1,22 @@
 // Switch languages of website
+// By replacing all page content
 function changeLanguage(e) {
     currentLanguage = e.target.id.split("-")[1];
+
     document.getElementById('content').innerHTML = null;
     populateSections();
     populateDevice();
     populateTech();
+}
+
+// By replacing the text only
+function changeLanguage_2(e) {
+    currentLanguage = e.target.id.split("-")[1];
+    let sectionNames = Object.keys(con_sectionDetails);
+
+    sectionNames.forEach(sectionId => {
+        let sectionDescContainer = document.getElementById(sectionId + '-desc-container');
+        sectionDescContainer.innerHTML = null;
+        sectionDescContainer.appendChild(customElement(con_sectionDetails[sectionId].structure["0"]))
+    });
 }
