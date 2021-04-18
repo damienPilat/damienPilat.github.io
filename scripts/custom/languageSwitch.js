6fd1391d3ed3a0ded4d2c1c9d7517a550909fc41
damienPilat <dspilat@hotmail.com>
2021-04-18 19:03:54 +0100

    Index Update: language Switch

--- scripts/custom/languageSwitch.js
+++ scripts/custom/languageSwitch.js
@@ -4,13 +4,20 @@ function changeLanguage(e) {
     // Update current language
     currentLanguage = e.target.id.split("-")[1];
 
+    updateSectionTextContent();
+    toggleClassNamesIfPresent('lang-btn', ['lang-btn-selected_light', 'lang-btn-selected_dark'], true);
+
+}
+
+// Update text content for each section
+function updateSectionTextContent() {
     // Get sections Names
-    let sectionNames = Object.keys(sectionDetails);
+    let sectionNames = Object.keys(con_sectionDetails);
 
     // Loop through section names,
     // Get section Desc containers, replace w/ sectionDetails path (will retrieve new language setting)
     sectionNames.forEach(sectionId => {
         let sectionDescContainer = document.getElementById(sectionId + '-desc-container');
-        sectionDescContainer.replaceWith(customElement(sectionDetails[sectionId].structure["0"].children["0"]));
+        sectionDescContainer.replaceWith(customElement(con_sectionDetails[sectionId].structure["0"].children["0"]));
     });
 }
