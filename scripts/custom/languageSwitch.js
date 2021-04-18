e96dbca03956f2a9ad6f9e0b4ad6ac4559650b37
damienPilat <dspilat@hotmail.com>
2021-04-18 19:07:09 +0100

    Index Update: variable renaming

--- scripts/custom/languageSwitch.js
+++ scripts/custom/languageSwitch.js
@@ -12,12 +12,12 @@ function changeLanguage(e) {
 // Update text content for each section
 function updateSectionTextContent() {
     // Get sections Names
-    let sectionNames = Object.keys(con_sectionDetails);
+    let sectionNames = Object.keys(sectionDetails);
 
     // Loop through section names,
     // Get section Desc containers, replace w/ sectionDetails path (will retrieve new language setting)
     sectionNames.forEach(sectionId => {
         let sectionDescContainer = document.getElementById(sectionId + '-desc-container');
-        sectionDescContainer.replaceWith(customElement(con_sectionDetails[sectionId].structure["0"].children["0"]));
+        sectionDescContainer.replaceWith(customElement(sectionDetails[sectionId].structure["0"].children["0"]));
     });
 }
