747b0ce1c3178ce0f318dda4af95a5003a5d6201
damienPilat <dspilat@hotmail.com>
2021-04-18 10:14:39 +0100

    Removed languageSwitch declaration in endScripts -> buttonOnClick

--- constructionFiles/scripts/custom/con_endScripts.js
+++ constructionFiles/scripts/custom/con_endScripts.js
@@ -7,9 +7,6 @@ function initiateEndScripts() {
     populateTech();
     // Enable pallet mode (light/dark)
     modeSwitch();
-    // Allow change content language
-// @todo:  Error -> languageSwitch not defined
-    languageSwitch();
 
     // Test: Tooltip library
     addTooltip();
