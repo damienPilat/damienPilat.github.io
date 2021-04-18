ed345e62fcf3a01bb21c1b6dbdb94f0f39feab19
damienPilat <dspilat@hotmail.com>
2021-04-18 11:58:52 +0100

    changeLanguage_v2 : correctly populates new text, div layout incorrect

--- constructionFiles/scripts/custom/con_languageSwitch.js
+++ constructionFiles/scripts/custom/con_languageSwitch.js
@@ -11,12 +11,19 @@ function changeLanguage(e) {
 
 // By replacing the text only
 function changeLanguage_2(e) {
+    // Update current language
     currentLanguage = e.target.id.split("-")[1];
+
+    // Get sections Names
     let sectionNames = Object.keys(con_sectionDetails);
 
+    // Loop through section names
     sectionNames.forEach(sectionId => {
+        // Get text container & empty
         let sectionDescContainer = document.getElementById(sectionId + '-desc-container');
         sectionDescContainer.innerHTML = null;
-        sectionDescContainer.appendChild(customElement(con_sectionDetails[sectionId].structure["0"]))
+
+        sectionDescContainer.appendChild(customElement(con_sectionDetails[sectionId].structure["0"].children["0"]));
+
     });
 }
