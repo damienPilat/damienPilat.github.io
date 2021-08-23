742fc889a1efcde8e6af81f96f362f8400ca77cd
damienPilat <dspilat@hotmail.com>
2021-08-23 22:21:07 +0100

    Activate Btns: BackToTop from Footre

--- scripts/custom/endScripts.js
+++ scripts/custom/endScripts.js
@@ -7,11 +7,37 @@ function initiateEndScripts() {
     populateTech();
     // Enable pallet mode (light/dark)
     modeSwitch();
+    // Create and Add EventListeners to Btns
+    activateBtns();
 
     // Test: Tooltip library
     addTooltip();
 
     collapsibleNavBar();
+}
+
+const activateBtns = () => {
+    // BackToTop From Footer
+    let btnContainer = document.getElementById('toTop-container');
+    console.log("btnContainer:", btnContainer);
+    // Append btn & img
+    btnContainer.appendChild(customElement({
+        domType: 'button',
+        id: 'toTop-btn',
+        children: {
+            0: {
+                domType: 'img',
+                src: 'menu-icon-v1.svg'
+            }
+        }
+    }));
+    // Back to top function
+    const backToTop = () => {
+        document.body.scrollTop = 0; // Safari
+        document.documentElement.scrollTop = 0 // Chrome, Firefox, IE, Opera
+    }
+    // Add 'click' event listener to btn#toTop w/ func backToTop()
+    addEventListenerWithId('toTop-btn', 'click', backToTop);
 }
 
 function addTooltip() {
