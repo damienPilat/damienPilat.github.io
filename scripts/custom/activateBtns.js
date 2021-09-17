d00321105551e23f0db03fa7d433dd4fe8df825c
damienPilat <dspilat@hotmail.com>
2021-09-17 22:27:24 +0100

    Update : 17/09/2021

--- scripts/custom/activateBtns.js
+++ scripts/custom/activateBtns.js
@@ -1,23 +1,59 @@
+
+
+// Add any event to a button, given an id
+const addEventToBtn = (elId, eventType, eventFunc) => {
+    addEventListenerWithId(elId, eventType, eventFunc)
+}
+
+// Append elements to a given ID
+const appendToElement = (elId, elementDetails) => {
+    let elementContainer = document.getElementById(elId);
+    elementContainer.appendChild(customElement(
+        elementDetails
+    ));
+    return elementContainer;
+}
+
+// ACTIVATE ALL BTNS IN DOM
 const activateBtns = () => {
-    // BackToTop From Footer
-    let btnContainer = document.getElementById('toTop-container');
-    console.log("btnContainer:", btnContainer);
-    // Append btn & img
-    btnContainer.appendChild(customElement({
-        domType: 'button',
-        id: 'toTop-btn',
-        children: {
-            0: {
-                domType: 'img',
-                src: '../media/icons/arrow-up.svg'
-            }
+    // BACK TO TOP FLOATING BTN
+    activateToTopFloatingBtn();
+}
+
+
+
+// Back To Top Floating Button
+const activateToTopFloatingBtn = () => {
+
+    // Add child to element
+    appendToElement('toTopFloating-container',
+        {
+            domType: 'img',
+            id: 'toTop-icon',
+            src: 'media/icons/arrow-up.svg'
+        });
+
+    // Enable element toggle
+    toggleAppearAfterXScroll('toTopFloating-container', 700)
+
+}
+
+// Toggle element appear after x scroll, with default fadeSpeed
+const toggleAppearAfterXScroll = (elId, scrollDistance, fadeSpeed=400) => {
+    //credits: https://stackoverflow.com/questions/11340789/make-an-element-visible-only-when-scrolled-down-to-px
+    let isVisible = false;
+    $(window).scroll(function(){
+        let shouldBeVisible = $(window).scrollTop()>scrollDistance;
+        if (shouldBeVisible && !isVisible) {
+            isVisible = true;
+            $('#'+elId).fadeIn(fadeSpeed);
+        } else if (isVisible && !shouldBeVisible) {
+            isVisible = false;
+            $('#'+elId).fadeOut(fadeSpeed);
         }
-    }));
-    // Back to top function
-    const backToTop = () => {
-        document.body.scrollTop = 0; // Safari
-        document.documentElement.scrollTop = 0 // Chrome, Firefox, IE, Opera
-    }
-    // Add 'click' event listener to btn#toTop w/ func backToTop()
-    addEventListenerWithId('toTop-btn', 'click', backToTop);
+    });
+
+    $('#'+elId).click(function(){
+        $(window).scrollTop(0);
+    });
 }
