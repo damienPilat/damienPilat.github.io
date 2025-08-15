939577b531142c3ea8502c6f6043a5360369ddaa
damienPilat <dspilat@hotmail.com>
2025-08-15 17:21:56 +0100

    deployment scripts

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/your-repository-name/' : '',
-  basePath: isProd ? '/your-repository-name' : '',
   output: 'export'
 }
 
